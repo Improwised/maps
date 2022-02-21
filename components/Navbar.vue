b6dba44e85f5051b699d77d2398dcc7979333c06
zeelrupapara <zeelrupapara@gmail.com>
2022-02-21 10:10:32 +0530

    style: add css in navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="nav-bar">
     <b-navbar>
-      <b-navbar-item class="ml-4 nav-heading"> Where in the world ? </b-navbar-item>
+      <b-navbar-item class="ml-4 nav-heading">
+        Where in the world ?
+      </b-navbar-item>
       <b-navbar-nav class="ml-auto">
         <b-nav-item class="mr-4">
-          <img class="moon" :src="require('@/static/moon-outline.svg')" alt="moon">
+          <img
+            class="moon"
+            :src="require('@/static/moon-outline.svg')"
+            alt="moon"
+          />
           <button class="darkmode">Dark Mode</button>
         </b-nav-item>
       </b-navbar-nav>
@@ -12,23 +18,33 @@
   </div>
 </template>
 <script>
-
-export default {};
+export default {}
 </script>
 
 <style scoped>
-.nav-bar{
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+
+.nav-bar {
   background-color: white;
+  box-shadow: rgb(100 100 111 / 20%) 0 7px 29px 0;
 }
 
-.darkmode{
+.darkmode {
   border-style: none;
-  background-color: #2B3743;
-  color: white;
+  color: #2b3743;
+  background-color:white;
+  font-weight: 600;
+  font-family: 'Nunito Sans', sans-serif;
 }
 
-.nav-heading{
-  color: white;
-  font-weight: bolder;
+.nav-heading {
+  color:#2b3743;
+  font-weight: 800;
+  font-family: 'Nunito Sans', sans-serif;
+  font-size: 17px;
+}
+
+.moon {
+  height: 25px;
 }
 </style>
